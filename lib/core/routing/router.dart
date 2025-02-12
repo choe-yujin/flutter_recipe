685cafe5a006edebd130563e023446ed6d688e29
choe-yujin <dev.yujinchoe@gmail.com>
2025-02-12 21:49:44 +0900

    feat: implement bottom navigation bar and routing (#Closes 49)
    - Added bottom navigation bar to the main screen
    - Configured routes for notifications, profile, home, and main screens
    - Updated route_paths.dart and router.dart for navigation management

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -4,12 +4,17 @@ import 'package:flutter_recipe/data/repository/mock_bookmark_repository_impl.dar
 import 'package:flutter_recipe/data/repository/mock_recipe_repository_impl.dart';
 import 'package:flutter_recipe/domain/model/recipe.dart';
 import 'package:flutter_recipe/domain/use_case/get_saved_recipes_use_case.dart';
+import 'package:flutter_recipe/presentation/main/main_screen.dart';
+import 'package:flutter_recipe/presentation/notifications/notifications_screen.dart';
+import 'package:flutter_recipe/presentation/profile/profile_screen.dart';
 import 'package:flutter_recipe/presentation/saved_recipes/screen/saved_recipes_root.dart';
 import 'package:flutter_recipe/presentation/saved_recipes/screen/saved_recipes_screen.dart';
 import 'package:flutter_recipe/presentation/sign_in/sign_in_screen.dart';
 import 'package:flutter_recipe/presentation/sign_up/sign_up_screen.dart';
 import 'package:flutter_recipe/presentation/splash/splash_screen.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../presentation/home/home_screen.dart';
 
 
 // GoRouter configuration
@@ -32,12 +37,56 @@ final router = GoRouter(
       path: RoutePaths.signIn,
       builder: (context, state) => SignInScreen(
         onTapSignUp: () => context.go(RoutePaths.signUp),
-        onTapSignIn: () => context.go(RoutePaths.savedRecipes),
+        onTapSignIn: () => context.go(RoutePaths.home),
       ),
     ),
-    GoRoute(
-      path: RoutePaths.savedRecipes,
-      builder: (context, state) => const SavedRecipesRoot(),
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return MainScreen(
+          body: navigationShell,
+          currentPageIndex: navigationShell.currentIndex,
+          onChangeIndex: (index) {
+            navigationShell.goBranch(
+              index,
+              initialLocation: index == navigationShell.currentIndex,
+            );
+          },
+        );
+      },
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.home,
+              builder: (context, state) => const HomeScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.savedRecipes,
+              builder: (context, state) => const SavedRecipesRoot(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.notifications,
+              builder: (context, state) => const NotificationsScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.profile,
+              builder: (context, state) => const ProfileScreen(),
+            ),
+          ],
+        ),
+      ],
     ),
   ],
 );
