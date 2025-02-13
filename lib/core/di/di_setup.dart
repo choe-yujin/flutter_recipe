4601cee37dd75114aa755ae3714ec29456dd6a32
choe-yujin <dev.yujinchoe@gmail.com>
2025-02-13 21:51:31 +0900

    refactor: SearchRoot with GetIt DI and run disetup in main.dart
    - Removed manual instantiation of use cases and view models.
    - Replaced with dependency injection using GetIt for cleaner, more maintainable code.
    - Updated SavedRecipesRoot and SearchRoot to get view models via GetIt.
    - Simplified the screen components by abstracting DI setup outside the screens, allowing for easier UI testing.

--- lib/core/di/di_setup.dart
+++ lib/core/di/di_setup.dart
@@ -1,19 +1,34 @@
 import 'package:flutter_recipe/data/data_source/recipe_data_source.dart';
 import 'package:flutter_recipe/data/data_source/recipe_data_source_impl.dart';
+import 'package:flutter_recipe/data/data_source/search_data_source.dart';
+import 'package:flutter_recipe/data/data_source/search_data_source_impl.dart';
 import 'package:flutter_recipe/data/repository/mock_bookmark_repository_impl.dart';
 import 'package:flutter_recipe/data/repository/mock_recipe_repository_impl.dart';
+import 'package:flutter_recipe/data/repository/recent_search_recipe_repository_impl.dart';
 import 'package:flutter_recipe/domain/repository/bookmark_repository.dart';
+import 'package:flutter_recipe/domain/repository/recent_search_recipe_repository.dart';
 import 'package:flutter_recipe/domain/repository/recipe_repository.dart';
 import 'package:flutter_recipe/domain/use_case/get_saved_recipes_use_case.dart';
+import 'package:flutter_recipe/domain/use_case/search_recipes_use_case.dart';
 import 'package:flutter_recipe/presentation/saved_recipes/saved_recipes_view_model.dart';
+import 'package:flutter_recipe/presentation/search/search_view_model.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 void diSetup() {
   // data source, repository 인터페이스 구현체. 타입 명시해주기
+  getIt.registerSingleton<SearchDataSource>(
+    SearchDataSourceImpl(),
+  );
   getIt.registerSingleton<RecipeDataSource>(
     RecipeDataSourceImpl(),
+  );
+
+  getIt.registerSingleton<RecentSearchRecipeRepository>(
+    RecentSearchRecipeRepositoryImpl(
+      searchDataSource: getIt(),
+    ),
   );
   getIt.registerSingleton<RecipeRepository>(
     MockRecipeRepositoryImpl(
@@ -32,11 +47,25 @@ void diSetup() {
     ),
   );
 
+  getIt.registerSingleton(
+    SearchRecipesUseCase(
+      recipeRepository: getIt(),
+      recentSearchRecipeRepository: getIt(),
+    ),
+  );
+
   // ViewModel은 상태를 유지하거나 변경하는 객체이기 때문에 각 화면마다 별개의 인스턴스를 생성해야 하기에 Factory로
   // ViewModel을 요청할 때마다 새로운 인스턴스를 생성하고, ViewModel이 필요로 하는 의존성(UseCase)을 GetIt에서 가져와서 주입
   getIt.registerFactory<SavedRecipesViewModel>(
     () => SavedRecipesViewModel(
       getSavedRecipesUseCase: getIt(),
+    ),
+  );
+
+  getIt.registerFactory<SearchViewModel>(
+    () => SearchViewModel(
+      recentSearchRecipeRepository: getIt(),
+      searchRecipesUseCase: getIt(),
     ),
   );
 }
