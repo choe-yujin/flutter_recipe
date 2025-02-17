8887dd2d9d6178e2a4fa97e301fb3b9de229d494
choe-yujin <dev.yujinchoe@gmail.com>
2025-02-17 18:47:07 +0900

    feat: implement filter-based search functionality
    - Add FilterState parameter to SearchRecipesUseCase
    - Update SearchViewModel to store query state
    - Modify search logic to consider both query and filter state

--- lib/domain/use_case/search_recipes_use_case.dart
+++ lib/domain/use_case/search_recipes_use_case.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_recipe/domain/model/recipe.dart';
 import 'package:flutter_recipe/domain/repository/recent_search_recipe_repository.dart';
 import 'package:flutter_recipe/domain/repository/recipe_repository.dart';
+import 'package:flutter_recipe/presentation/search/filter_state.dart';
 
 class SearchRecipesUseCase {
   final RecipeRepository _recipeRepository;
@@ -12,19 +13,40 @@ class SearchRecipesUseCase {
   })  : _recipeRepository = recipeRepository,
         _recentSearchRecipeRepository = recentSearchRecipeRepository;
 
-  Future<List<Recipe>> execute(String query) async {
+  Future<List<Recipe>> execute(
+    String query,
+    FilterState filterState,
+  ) async {
     if (query.isEmpty) {
       return [];
     }
 
     final results = (await _recipeRepository.getRecipes())
         .where((e) => e.name.toLowerCase().contains(query.toLowerCase()))
-        .toList();
+        .where(
+          (e) {
+            if (filterState.time == 'All') {
+              return true;
+            } else {
+              return filterState.time == e.time;
+            }
+          },
+        )
+        .where((e) => e.rating >= filterState.rate)
+        .where((e) {
+          if (filterState.category == 'All') {
+            return true;
+          } else {
+            return filterState.category == e.category;
+          }
+        });
 
     // 최근 검색 결과를 메모리에 저장
     //print('Updating recent search recipes: ${results.length} items');
-    await _recentSearchRecipeRepository.updateRecentSearchRecipes(results);
+    _recentSearchRecipeRepository.updateRecentSearchRecipes(
+      results.toList(),
+    );
 
-    return results;
+    return results.toList();
   }
 }
--- lib/presentation/search/search_view_model.dart
+++ lib/presentation/search/search_view_model.dart
@@ -25,34 +25,36 @@ class SearchViewModel with ChangeNotifier {
     _state = state.copyWith(isLoading: true);
     notifyListeners();
 
-    final recentSearches =
-        await _recentSearchRecipeRepository.getRecentSearchRecipes();
-
-    //print('Loaded recent searches: ${recentSearches.length} items');
-
     _state = state.copyWith(
-      recipes: recentSearches,
+      recipes: await _recentSearchRecipeRepository.getRecentSearchRecipes(),
       isLoading: false,
     );
     notifyListeners();
   }
 
   // 검색 메서드
-  void searchRecipes(String query) async {
+  Future<void> searchRecipes(String query) async {
     _state = state.copyWith(isLoading: true);
     notifyListeners();
 
-    final results = await _searchRecipesUseCase.execute(query);
+    final recipes = await _searchRecipesUseCase.execute(
+      query,
+      state.filterState,
+    );
 
     _state = state.copyWith(
-      recipes: results,
+      recipes: recipes,
       isLoading: false,
+      searchTitle: 'Search Result',
+      resultsCount: '${recipes.length} results',
+      query: query,
     );
     notifyListeners();
   }
 
   void onChangeFilter(FilterState filterState) async {
     _state = state.copyWith(filterState: filterState);
+    await searchRecipes(state.query);
     notifyListeners();
 
     print(state.toString());
